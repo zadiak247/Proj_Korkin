c5010af29bf681c786b887e0fb6cd06ac0b12c30
ALKME61 <owo50531@gmail.com>
2023-11-21 17:49:19 +0300

    PZ_6 add

---
+++ PZ_6/PZ_6.1.py
@@ -0,0 +1,29 @@
+"""
+Дан список A размера N (N - нечетное число). Вывести его элементы с нечетными номерами в порядке убывания номеров:
+Aₙ, Aₙ₋₂, Aₙ₋₄, ..., A₁. Условный оператор не использовать.
+"""
+
+import random
+
+N = random.randint(-1000, 1000)
+while N % 2 == 0:
+    N = random.randint(-1000, 1000)
+
+A = []
+i = 0
+while i < abs(N):
+    A.append(random.randint(-1000, 1000))
+    i += 1
+
+_A = []
+i = 0
+while i <= abs(N):
+    try:
+        while i % 2 != 0:
+            _A.append(A[-i])
+            break
+    except IndexError:
+        break
+    i += 1
+
+print('элементы списка A с нечетными номерами (в порядке убывания номеров): ', '\n', _A)
