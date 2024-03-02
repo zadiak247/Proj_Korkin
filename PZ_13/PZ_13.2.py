2c7156601ca54fcc75cb2f979199367a0c4abe1a
owo390 <owo50531@gmail.com>
2024-03-02 14:39:46 +0300

    PZ_13 fix

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -3,13 +3,13 @@
 """
 import random
 
-matrix = [[random.randint(-20, 20) for x in range(-10, 10)] for y in range(1, 6)]
+matrix = [[random.randint(-20, 20) for x in range(-5, 5)] for y in range(1, 6)]
 
 
 def func():
     mas = []
     [mas.append(matrix[i][z]) for i in range(len(matrix)) for z in range(len(matrix[i])) if matrix[i][z] < 0]
-    yield f'Новый массив, содержащий отрицательные элементы матрицы: {mas}'
+    yield f'Новый массив, содержащий отрицательные элементы матрицы: {mas}\nРазмер полученного массива: {len(mas)}'
 
 
 [print(f'Исходная матрица: {matrix}\n', i) for i in func()]
